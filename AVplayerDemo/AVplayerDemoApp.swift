4f86b40c0e795d77295d9304635547f9fedab83e
Vishal Kothari <vishal.chikki@gmail.com>
2025-12-20 14:09:40 +0530

    Initial Commit

---
+++ AVplayerDemo/AVplayerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVplayerDemoApp.swift
+//  AVplayerDemo
+//
+//  Created by Vishal Kothari on 20/12/25.
+//
+
+import SwiftUI
+
+@main
+struct AVplayerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
